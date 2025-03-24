4948c870bc32ac6857c8c4a89e699c045f5ee263
jvitor <jvitor.pinheiro.garcia@gmail.com>
2025-03-24 20:56:23 -0300

    hello

---
+++ exercicios/Exercicio01CicloDeVida/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from "./styles";
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>{}
+      <View style={styles.box}>{}
+        <Text style={styles.boxText}>Isto é ima caixa</Text>
+      </View>    
+    </View>
+  );
+}
+
