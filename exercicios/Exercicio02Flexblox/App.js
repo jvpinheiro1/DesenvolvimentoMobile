e36664ab487e9a45b6547ce2bc040ef354c27c1c
jvitor <jvitor.pinheiro.garcia@gmail.com>
2025-03-24 21:21:23 -0300

    Exercicios flexBox

--- exercicios/Exercicio02Flexblox/App.js
+++ exercicios/Exercicio02Flexblox/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View} from "react-native";
+import styles from "./styles";
+import Box from "./Box";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Box>#1</Box>
+      <Box>#2</Box>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
